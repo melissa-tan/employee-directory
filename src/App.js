5d169775bd31564b99135e5418ee1e2331035f94
melissa-tan <melissatan9683@gmail.com>
2021-07-03 11:45:40 -0700

    Axios and fetch data working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Table from './components/Table';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get('https://randomuser.me/api/?results=20');
+            setEmployees(request.data.results);
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <>
+            <Header />
+            <Table employees={employees}/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Table({ employees }) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>First</th>
+                        <th>Last</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody >
+                    {employees && employees.length > 0 ? (
+
+                        employees.map((employee) => (
+                            <tr key={employee.login.uuid} >
+                                <td>
+                                    <img className={'headshot'} alt={employee.name.last} src={employee.picture.medium} />
+                                </td>
+                                <td>{employee.name.first}</td>
+                                <td>{employee.name.last}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.phone}</td>
+                                <td>{employee.location.country}</td>
+                            </tr>
+                        ))
+                    ) : (<h1> No Results Found! </h1>)
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
