12ccac2e327068f7113f53d4d8c8ec3a66a7be93
melissa-tan <melissatan9683@gmail.com>
2021-07-05 16:39:00 -0700

    sort by asc and desc working

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import { faSort } from '@fortawesome/free-solid-svg-icons'
 
 export default function Table({ employees }) {
+    const [sortState, setSortState] = useState(true);
+    const [sortedEmployees, setSortedEmployees] = useState([]);
+
+    const sort = (column) => {
+        const columnArray = column.split('.');
+        const current =
+            sortedEmployees.length > 0 ? sortedEmployees : employees;
+        const updateSort = current.sort((a, b) => {
+            const nameA =
+                columnArray.length === 1
+                    ? a[column]
+                    : a[columnArray[0]][columnArray[1]];
+            const nameB =
+                columnArray.length === 1
+                    ? b[column]
+                    : b[columnArray[0]][columnArray[1]];
+            if (nameA < nameB) {
+                return sortState ? -1 : 1;
+            }
+            if (nameA > nameB) {
+                return sortState ? 1 : -1;
+            }
+            return 0;
+        });
+
+        setSortedEmployees(updateSort);
+        setSortState(!sortState);
+    };
 
     return (
         <div>
@@ -10,22 +38,24 @@ export default function Table({ employees }) {
                 <thead>
                     <tr>
                         <th>Image</th>
-                        <th onClick={(event) => console.log(event)} className={'hover'}>First&nbsp;
-                            <FontAwesomeIcon icon={ faFilter } /></th>
-                        <th onClick={(event) => console.log(event)} className={'hover'}>Last&nbsp;
-                            <FontAwesomeIcon icon={ faFilter } /></th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Location</th>
+                        <th onClick={() => sort('name.first')} className={'hover'}>First&nbsp;
+                            <FontAwesomeIcon icon={faSort} /></th>
+                        <th onClick={() => sort('name.last')} className={'hover'}>Last&nbsp;
+                            <FontAwesomeIcon icon={faSort} /></th>
+                        <th onClick={() => sort('email')} className={'hover'}>Email&nbsp;
+                            <FontAwesomeIcon icon={faSort} /></th>
+                        <th onClick={() => sort('phone')} className={'hover'}>Phone&nbsp;
+                            <FontAwesomeIcon icon={faSort} /></th>
+                        <th onClick={() => sort('location.country')} className={'hover'}>Location&nbsp;
+                            <FontAwesomeIcon icon={faSort} /></th>
                     </tr>
                 </thead>
-                <tbody >
+                <tbody>
                     {employees && employees.length > 0 ? (
-
-                        employees.map((employee) => (
-                            <tr key={employee.login.uuid} >
+                        employees.map((employee, index) => (
+                            <tr key={employee.login.uuid} className={index % 2 === 0 ? 'odd' : 'even'}>
                                 <td>
-                                    <img className={'headshot'} alt={employee.name.last} src={employee.picture.medium} />
+                                    <img alt={employee.name.last} src={employee.picture.medium} />
                                 </td>
                                 <td>{employee.name.first}</td>
                                 <td>{employee.name.last}</td>
@@ -34,8 +64,13 @@ export default function Table({ employees }) {
                                 <td>{employee.location.country}</td>
                             </tr>
                         ))
-                    ) : (<h1> No Results Found! </h1>)
-                    }
+                    ) : (
+                        <tr>
+                            <td colSpan={6} className={'noResults'}>
+                                No Results Found!
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
